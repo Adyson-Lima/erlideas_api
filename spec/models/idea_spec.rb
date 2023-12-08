40386a1faab31e9089c7d31dc850d163052a9f3f
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-08 19:24:09 -0300

    3° commit - tdd model: name consegue ser preenchido? GREEN

--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Idea, type: :model do
   describe 'Testes de preenchimento do model Idea' do
 
     it 'name consegue ser preenchido?' do
-      @idea.name = ''
+      @idea.name = 'bif'
       expect(@idea.name).to eq('bif')
     end
 
